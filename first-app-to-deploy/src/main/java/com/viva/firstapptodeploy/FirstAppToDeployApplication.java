56b310cdd7ba593bf4264fc080b6108b8c7d0f80
puneetvashisht@gmail.com <puneetvashsiht@gmail.com>
2020-02-17 14:48:12 +0530

    first commit

---
+++ first-app-to-deploy/src/main/java/com/viva/firstapptodeploy/FirstAppToDeployApplication.java
@@ -0,0 +1,13 @@
+package com.viva.firstapptodeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstAppToDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstAppToDeployApplication.class, args);
+	}
+
+}
---
+++ first-app-to-deploy/src/test/java/com/viva/firstapptodeploy/FirstAppToDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viva.firstapptodeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstAppToDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
